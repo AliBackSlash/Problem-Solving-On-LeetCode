55be0eaebad5374ffe5a398ba9d820828d6e915a
Ali Elsaied <ae1378032@gmail.com>
2024-12-22 11:39:21 +0200

    Time: 58 ms (11.44%), Space: 48.1 MB (40.64%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cs
@@ -0,0 +1,26 @@
+public class Solution
+ {
+    public int[] TwoSum(int[] nums, int target)
+     {
+        
+        int[] indices = new int[2];
+         for (int i = 0; i < nums.Length; i++)
+          {
+           for (int j = 0; j < nums.Length; j++)
+           {
+              if(i!=j)
+ {
+     if (nums[i] + nums[j] == target)
+     {
+         indices[0] = i;
+         indices[1] = j;
+         return indices;
+     }
+ }
+     }
+ }
+
+ return nums;
+
+    }
+}
