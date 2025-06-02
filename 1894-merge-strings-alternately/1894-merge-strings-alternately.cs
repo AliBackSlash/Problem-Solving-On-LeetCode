08d4db223896922248996d6068948416c547ebd7
Ali Elsaied <ae1378032@gmail.com>
2025-06-02 19:41:04 +0300

    Time: 53 ms (97.45%), Space: 40.4 MB (52.83%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.cs
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.cs
@@ -1,20 +1,19 @@
 public class Solution {
+
     public string MergeAlternately(string word1, string word2)
 {
-    StringBuilder s = new StringBuilder();
-    byte Length = Convert.ToByte(word1.Length + word2.Length);
-    for (int i = 0;i<Length-1;i++)
-    {
-        if (word1.Length > i && word2.Length > i)
-            s.Append($"{word1[i]}{word2[i]}");
-        else if (word1.Length >= i && word2.Length <= i)
-            return s.Append($"{word1.Substring(i)}").ToString();
-        else if (word1.Length <= i && word2.Length >= i)
-            return s.Append($"{word2.Substring(i)}").ToString();
-        else
-            return s.ToString();
+    StringBuilder result = new ();
 
+    int len = word1.Length < word2.Length ? word1.Length : word2.Length;
+    int i;
+    for (i = 0; i < len; i++)
+    {
+        result.Append(word1[i].ToString() + word2[i].ToString());
     }
-    return s.ToString();
+
+    if (word1.Length != word2.Length)
+        result.Append(word1.Length > word2.Length ? word1.Substring(i).ToString() : word2.Substring(i).ToString());
+
+    return result.ToString();
 }
 }
