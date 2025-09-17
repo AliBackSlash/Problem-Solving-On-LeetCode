7d461452685f4505e2474d8b8751feadd2fdc1ec
Ali Elsaied <ae1378032@gmail.com>
2025-09-17 17:37:22 +0300

    Time: 335 ms (43.65%), Space: 62.3 MB (58.33%) - LeetHub

--- 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.cs
+++ 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.cs
@@ -1,26 +1,21 @@
 public class Solution {
     public int MaxOperations(int[] nums, int k) {
-         int len = nums.Length;
- if(len < 2) return 0;
-
- Array.Sort(nums);
-
- int L=0, R=len-1;
- int Count = 0;
- int SumOfRAndL = 0;
- while (L < R) {
-
-     SumOfRAndL = nums[L] + nums[R];
-     if (SumOfRAndL == k)
-     { Count++; nums[L] = nums[R] = -1; L++; R--; }
-     else if (SumOfRAndL > k)              
-         R--;
-     
-     else 
-         L++;
-
- }
-
- return Count;
+        int MaxOper = 0, l = 0,sum, r = nums.Length - 1;
+        Array.Sort(nums);      
+        while (l < r)
+        {
+            sum = nums[l] + nums[r];
+            if (sum == k)
+            {
+                MaxOper++;
+                l++;
+                r--;
+            }
+            else if (sum > k)
+                r--;
+            else
+                l++;
+        }
+        return MaxOper;
     }
 }
